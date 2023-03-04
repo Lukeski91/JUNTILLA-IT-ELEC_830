9c0667d6372ef2aa3c3f697b81afa9b0449cd7bb
Christopher Luke M. Juntilla <cljuntilla91@gmail.com>
2023-03-04 12:24:48 +0800

    Christopher Luke M. Juntilla

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     public static final String TAG = "MainActivity";
     @Override
