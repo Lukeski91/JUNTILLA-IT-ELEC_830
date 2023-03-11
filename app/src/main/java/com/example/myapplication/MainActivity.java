42247a1d166ee5fe9017665c7ae36a44f5d67cb3
Christopher Luke Juntilla <cljuntilla91@gmail.com>
2023-03-11 12:21:24 +0800

    03-11-2023

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         secondFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadFragment(new FragmentB());
+                loadFragment(new FragmentA());
             }
         });
     }
