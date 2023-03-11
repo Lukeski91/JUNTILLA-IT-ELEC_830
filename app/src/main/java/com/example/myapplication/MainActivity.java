160b560ea6cadd1867152023cc4e7b7f12b80cf1
Christopher Luke Juntilla <cljuntilla91@gmail.com>
2023-03-11 11:41:51 +0800

    03-11-2023

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button firstFragment, secondFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstFragment = (Button) findViewById(R.id.button);
+        secondFragment = (Button) findViewById(R.id.button2);
     }
 }
+//
