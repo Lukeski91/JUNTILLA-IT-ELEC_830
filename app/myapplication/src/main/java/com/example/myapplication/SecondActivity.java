275b38be47213bbd9cd07dc5a20b273c14aab1ad
Christopher Luke M. Juntilla <cljuntilla91@gmail.com>
2023-04-15 12:24:10 +0800

    04/15/2023

---
+++ app/myapplication/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.example.myapplication.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ActivitySecondBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //TextView textView = (TextView) findViewById(R.id.text);
+
+        Intent intent = getIntent();
+        String fromMain = getIntent().getStringExtra("PASS_ME");
+    }
+
+}
